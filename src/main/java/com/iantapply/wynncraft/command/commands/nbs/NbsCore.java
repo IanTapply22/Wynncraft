6e43d68bb058adf18b4b0df33478913fd3155743
IanTapply22 <iantapply22@gmail.com>
2024-12-02 15:56:59 -0500

    Add NBS command core initialize method comment

--- src/main/java/com/iantapply/wynncraft/command/commands/nbs/NbsCore.java
+++ src/main/java/com/iantapply/wynncraft/command/commands/nbs/NbsCore.java
@@ -6,6 +6,9 @@ import com.iantapply.wynncraft.nbs.players.PlayerOrientedSongPlayer;
 public class NbsCore {
     public static PlayerOrientedSongPlayer player;
 
+    /**
+     * Stages all NBS related commands in the current instance of the command manager
+     */
     public void initialize() {
         Wynncraft.getInstance().getCommandCore().stageCommand(new NbsCommand());
     }
