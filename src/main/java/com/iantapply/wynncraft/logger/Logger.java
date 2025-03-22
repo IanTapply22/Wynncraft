e082a88fe9d75309b0d26b5ab75477ec4a2154a6
IanTapply22 <iantapply22@gmail.com>
2025-03-21 21:00:12 -0400

    Remove logger deprecation
    Fixes #26

--- src/main/java/com/iantapply/wynncraft/logger/Logger.java
+++ src/main/java/com/iantapply/wynncraft/logger/Logger.java
@@ -8,15 +8,6 @@ import org.bukkit.Bukkit;
  * A utility class intended to log messages easily to the Bukkit console
  */
 public class Logger {
-    /**
-     * Deprecated method to log a message to the console with STDOUT.
-     * The preferred method to log is with the log method which uses the Adventure API.
-     * @param message The message to log
-     */
-    @Deprecated
-    public static void logWithSTDOUT(String message) {
-        System.out.println(message);
-    }
 
     /**
      * Log a message to the console
